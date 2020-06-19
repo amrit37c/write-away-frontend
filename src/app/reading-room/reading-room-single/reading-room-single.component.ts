2e6a8f235f729529c0e39187e7ca43dcba58f2de
Harjot <jot143@live.in>
2020-06-19 17:45:18 +0530

    main reading page dynamic

--- src/app/reading-room/reading-room-single/reading-room-single.component.ts
+++ src/app/reading-room/reading-room-single/reading-room-single.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { volumes } from './reading-model';
 @Component({
   selector: 'app-reading-room-single',
   templateUrl: './reading-room-single.component.html',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadingRoomSingleComponent implements OnInit {
 
+  currentVol = 0;
+  currentTopic = null;
+
+  reading = {
+      "publication": {
+        'title'    : "WRITING PROMPTS FOR GRADES",
+        'genres'   : ['Fantasy', 'Classic'],
+        "created_at"  : "June 24, 2020",
+        "rating"   : 3,
+        "reads"    : 1200
+      },
+      "volumes": volumes
+  }
   constructor() { }
 
   ngOnInit() {
+    console.log(this.reading);
   }
 
+  increment() {
+    if(this.currentVol < this.reading.volumes.length - 1){
+      this.currentTopic = null;
+      this.currentVol++;
+    } 
+    
+  }
+
+  decrement() {
+    if(this.currentVol > 0) {
+      this.currentVol--;
+      this.currentTopic = null;
+    } 
+  }
+
+  loadTopic(topic) {
+    this.currentTopic = topic;
+  }
 }
